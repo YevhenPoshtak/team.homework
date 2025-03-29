81b162b7db555fd2e283bcd563a70af690c09b15
Yaroslava <yakrenevych@gmail.com>
2025-03-30 00:43:06 +0200

    updated background

--- Background.py
+++ Background.py
@@ -1,17 +1,19 @@
 import turtle
-import time
-import math
 import random
 
 turtle.setup(500, 500)
 turtle.tracer(0)
 screen = turtle.Screen()
+list_stars = []
+
+for _ in range(20):
+    x = random.randint(-250, 250)
+    y = random.randint(-250, 250)
+    list_stars.append((x, y))
 
 
 class Background:
-
     def __init__(self):
-
         self.pen = turtle.Turtle()
         self.pen.speed(0)
         self.pen.hideturtle()
@@ -20,9 +22,23 @@ class Background:
         self.pen.clear()
         if 6 <= hour < 18:
             screen.bgcolor("sky blue")
+            self.pen.up()
+            self.pen.goto(200, -200)
+            self.pen.dot(50, 'yellow')
+            self.pen.dot(25, 'orange')
         else:
             screen.bgcolor("midnight blue")
-            for _ in range(10):
+            for el in list_stars:
                 self.pen.penup()
-                self.pen.goto(random.randint(-250, 250), random.randint(-250, 250))
+                self.pen.goto(*el)
                 self.pen.dot(random.randint(2, 5), "white")
+                self.pen.goto(200, -200)
+                self.pen.dot(50, 'white')
+                self.pen.goto(220, -200)
+                self.pen.dot(50, 'midnight blue')
+
+
+if __name__ == '__main__':
+    background = Background()
+    background.draw(20)
+    turtle.mainloop()
