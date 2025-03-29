4d4cbde0afa8914ce41bd47eb0dacf6f92a9bcf4
Yaroslava <yakrenevych@gmail.com>
2025-03-30 01:03:03 +0200

    updated and checked Dial

--- Dial.py
+++ Dial.py
@@ -1,5 +1,4 @@
 import turtle
-import time
 import math
 import random
 from Digit import Digit
@@ -9,34 +8,33 @@ class Dial:
     def __init__(self, radius):
         self.radius = radius
         self.digits = []
-        self.stars = []
 
         for i in range(1, 13):
             angle = (12 - i) * 30
             angle %= 360
             if i not in [12, 6]:
                 angle = (360 - angle) % 360
-            digit = Digit(i, angle, radius)
+            digit = Digit(i, angle, radius + 20)
             self.digits.append(digit)
 
-        for _ in range(12):
-            angle = random.uniform(0, 360)
-            x = (self.radius + 20) * math.cos(math.radians(angle))
-            y = (self.radius + 20) * math.sin(math.radians(angle))
-            self.stars.append((x, y))
 
-    def draw(self, turtle_obj, offset_x=0, offset_y= 120):
+    def draw(self, turtle_obj, offset_x=0, offset_y=0):
         turtle_obj.penup()
-        turtle_obj.goto(offset_x + self.radius, offset_y - self.radius/2 )
+        turtle_obj.goto(offset_x, offset_y - self.radius)
         turtle_obj.pendown()
         turtle_obj.pensize(8)
         turtle_obj.color("gold")
         turtle_obj.circle(self.radius)
 
-        for x, y in self.stars:
-            turtle_obj.penup()
-            turtle_obj.goto(x + offset_x, y + offset_y)
-            turtle_obj.dot(3, "yellow")
-
         for digit in self.digits:
             digit.draw(turtle_obj, offset_x, offset_y)
+
+
+if __name__ == '__main__':
+    pen = turtle.Turtle()
+    screen = turtle.Screen()
+    screen.bgcolor("sky blue")
+
+    dial = Dial(90)
+    dial.draw(pen, 0, 0)
+    turtle.mainloop()
